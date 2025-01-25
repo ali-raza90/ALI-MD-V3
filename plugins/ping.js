b020e7b42c69bee5c35ef4c0a5bc1a4bdea4acef
𝐀𝐋𝐈ャ <ar6167423@gmail.com>
2025-01-25 10:59:39 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -27,7 +27,7 @@ l(e)
 
 cmd({
     pattern: "ping",
-    react: "🍁",
+    react: "♻️",
     alias: ["speed"],
     desc: "Check bot\'s ping",
     category: "main",
@@ -37,12 +37,12 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '*_🍭Pinging..._*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*☇ ᴀʟɪ ꜱᴩᷨᴇͦᴇͭᴅ 🐍 : ${ping} ᴍꜱ*`}, { quoted: message })
+        await conn.sendMessage(from, { text: `*♻️ Speed... : ${ping}ms*`}, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
     }
 })
+                    
